72ed86ecd9cf648d384a6ec34fe24c5e2ba3b416
samadii <77415087+samadii@users.noreply.github.com>
2021-08-01 05:08:20 +0430

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
     
 class Config:
-    bot_api = os.environ.get("BOT_TOKEN", None)
+    bot_api = os.environ.get("BOT_TOKEN")
     DEFAULT_VOTE_COUNT = int(os.environ.get("DEFAULT_VOTE_COUNT", 10))
     DEFAULT_DELETE_TIMEOUT = int(os.environ.get("DEFAULT_DELETE_TIMEOUT", 60))
     DB_URI = os.environ.get("DB_URI", 'sqlite:///db.db')
